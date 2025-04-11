f832134a40c6ce465963e3ad9fbaa5f671b897f3
Nikola Novkovic <nikola.novkovic@paprikart.co.rs>
2025-04-11 23:11:40 +0200

    initialized header and characters page

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { CharactersService } from '../../services/characters.service';
+import { Character, CharacterRes } from '../common/models/character.model';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  
+}
