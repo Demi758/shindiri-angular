5ccbbb39dbf17ed960ea3db7dcd20653cada9971
Nikola Novkovic <nikola.novkovic@paprikart.co.rs>
2025-04-11 23:55:42 +0200

    displayed all characters on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CharacterCardComponent } from './components/home/character-card/character-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    CharacterCardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,17 @@ import { Character, CharacterRes } from '../common/models/character.model';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  
+export class HomeComponent implements OnInit {
+  constructor(private characterService: CharactersService) {}
+
+  characters?: Character[] = [];
+
+  ngOnInit() {
+    this.characterService.getCharacters().subscribe({
+      next: (res: CharacterRes) => {
+        const resCharacters = res.results;
+        this.characters = resCharacters?.map((character: Character) => new Character(character));
+      },
+    });
+  }
 }
